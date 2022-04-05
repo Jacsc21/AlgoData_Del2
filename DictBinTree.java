c5507bef465042b28db355f1142aa3fe2f85e5c4
ArktosJ <julian.soerensen@outlook.dk>
2022-04-05 14:01:07 +0200

    DictBinTree v1

--- DictBinTree.java
+++ DictBinTree.java
@@ -1,3 +1,42 @@
-public class DictBinTree {
+public class DictBinTree implements Dict{
     
+    private BinNode root;
+
+    public DictBinTree(){
+        BinNode root = null;
+    }
+
+    public void insert(int k){
+        BinNode y = null;
+        BinNode x = this.root;
+        while(x != null){
+            y = x;
+            if(z.getData() < x.getData()){
+                x = x.getLeftChild();
+            
+            }
+            else x = x.getRightChild();
+        }
+        if(y == null){
+            this.root = new BinNode(k);
+        }
+        else if(k < y.getData())
+        y.setLeftChild(new BinNode(k));
+        else y.setRightChild(new BinNode(k));
+    }
+
+    public boolean search(int k){
+        BinNode x = this.root;
+        Boolean result = false;
+        while(k != x.getData() || x == null){
+            if(x == null)
+            result = false;
+            if(k == x.getData())
+            result = true;
+            if(k < x.getData())
+            x = x.getLeftChild();
+            else x = x.getRightChild();
+        }
+        return result; 
+    }
 }
